ea4867563a7bc928ed8f2880a67d7800f9d46b6c
Dridi Walid <wdridi1@gmail.com>
2018-01-14 01:49:39 +0100

    Init pci demo project

---
+++ tests/DemoTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+  public function test_pass()
+  {
+    $this->assertTrue(true);
+  }
+}
+
